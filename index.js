8b03404419bac1c0fce4c3ac0f1112721d4e836a
smartcatalogdev <smart.catalog.bd@gmail.com>
2021-06-01 16:12:44 +0600

    nodemon, environment variable and Route parameters

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-    response.send("Hello World")
+    response.send("Hello World v2")
 })
 
 app.get('/api/courses', (request, response) => {
     response.send([1,2,3,4,5])
 })
 
-app.listen(3000, () => console.log('listening on port 3000....'))
+app.get('/api/courses/:year/:id', (request, response) => {
+    // getting all params
+    // response.send(request.params)
+
+    // getting all query string
+    response.send(request.query)
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}....`))
